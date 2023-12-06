a2e388243f26d2329e8d4c19067ab6d22c516819
Anshu-neo-jha <neo529916@gmail.com>
2023-12-07 00:18:34 +0530

    update

--- todo_app/serializers.py
+++ todo_app/serializers.py
@@ -1,7 +1,32 @@
+
 from rest_framework import serializers
-from .models import TodoItem
+from .models import TodoItem, Tag
+
+class TagSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = ['id', 'name']
 
 class TodoItemSerializer(serializers.ModelSerializer):
+    tags = TagSerializer(many=True, required=False)
+
     class Meta:
         model = TodoItem
-        fields = '__all__'
+        fields = ['id', 'title', 'description', 'due_date', 'tags', 'status']
+
+    @staticmethod
+    def get_existing_tags(tags_data):
+        existing_tags = []
+        for tag_data in tags_data:
+            tag_name = tag_data['name']
+            tag_instance, created = Tag.objects.get_or_create(name=tag_name)
+            if not created:
+                existing_tags.append({'id': tag_instance.id, 'name': tag_instance.name})
+        return existing_tags
+
+    def create(self, validated_data):
+        tags_data = validated_data.pop('tags', [])
+        todo_item = TodoItem.objects.create(**validated_data)
+        tags_instances = [Tag.objects.get_or_create(name=tag_data['name'])[0] for tag_data in tags_data]
+        todo_item.tags.set(tags_instances)
+        return todo_item
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,11 +1,31 @@
+from .models import *
+from .serializers import *
 from rest_framework import generics
-from .models import TodoItem
-from .serializers import TodoItemSerializer
+from rest_framework.response import Response
+from rest_framework import status
+
+
+
+class TodoItemDetailView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = TodoItem.objects.all()
+    serializer_class = TodoItemSerializer
 
 class TodoItemListView(generics.ListCreateAPIView):
     queryset = TodoItem.objects.all()
     serializer_class = TodoItemSerializer
 
-class TodoItemDetailView(generics.RetrieveUpdateDestroyAPIView):
-    queryset = TodoItem.objects.all()
-    serializer_class = TodoItemSerializer
+    def create(self, request, *args, **kwargs):
+        create_new_tags = request.data.get('create_new_tags', True)  
+        tags_data = request.data.get('tags', [])
+        if not create_new_tags:
+            existing_tags = TodoItemSerializer.get_existing_tags(tags_data)
+            request.data['tags'] = existing_tags
+
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+
+        instance = serializer.save()
+
+        headers = self.get_success_headers(serializer.validated_data)
+        return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
+
